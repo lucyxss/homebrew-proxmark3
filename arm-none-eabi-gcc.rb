80080b3d21fa547826d2e3a22f3eff5b8200babb
lucy <lucy>
2022-02-16 19:24:38 +0800

    init

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -1,11 +1,15 @@
+
+
 class ArmNoneEabiGcc < Formula
   desc "GCC for embedded ARM processors"
-  homepage "https://launchpad.net/gcc-arm-embedded"
-  url "https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q3-update/+download/gcc-arm-none-eabi-5_4-2016q3-20160926-mac.tar.bz2"
-  version "20160926"
-  sha256 "5656cdec40f99d5c054a85bbc694276e1c4a1488cdacbbc448bc6acd3bbe070d"
+  homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads'
+  version '10-2020-q4-major'
+
+  url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-mac.tar.bz2'
+  sha256 'bed12de3565d4eb02e7b58be945376eaca79a8ae3ebb785ec7344e7e2db0bdc0'
 
   def install
-    cp_r ["arm-none-eabi", "bin", "lib", "share"], "#{prefix}/"
+    (prefix/"gcc").install Dir["./*"]
+    Dir.glob(prefix/"gcc/bin/*") { |file| bin.install_symlink file }
   end
 end
